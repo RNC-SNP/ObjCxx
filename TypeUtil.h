012e64e075b29c0b66b62f2b64d8546e2f4ba7b1
Rinc Liu <i@rinc.xyz>
2017-05-24 10:29:30 +0800

    Update TypeUtil.h

--- TypeUtil.h
+++ TypeUtil.h
@@ -13,6 +13,10 @@
 
 #define chars2NSString(chars) [NSString stringWithCString:chars encoding:NSUTF8StringEncoding]
 
+#define chars2NSData(chars) [[NSData alloc] initWithBytes:chars length:strlen(chars)]
+
 #define NSString2NSData(nsStr) [nsStr dataUsingEncoding:NSUTF8StringEncoding]
+
+#define NSData2NSString(nsData) [[NSString alloc] initWithData:nsData encoding:NSUTF8StringEncoding]
 
 #define int2NSString(i) [NSString stringWithFormat:@"%d", i]
