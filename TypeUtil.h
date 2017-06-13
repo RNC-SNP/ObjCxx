468bd774ad86631624593be46be4dc72ed620384
Rinc Liu <i@rinc.xyz>
2017-06-13 03:24:12 -0500

    Update TypeUtil.h

--- TypeUtil.h
+++ TypeUtil.h
@@ -5,20 +5,32 @@
 //  Copyright © 2017 RincLiu. All rights reserved.
 //
 
-#define NSStr2STDStr(nsStr) std::string([nsStr UTF8String])
+#define NSStr2CharPA(nsStr) (unsigned char*)[nsStr cStringUsingEncoding:NSNonLossyASCIIStringEncoding]
 
-#define STDStr2NSStr(stdStr) [NSString stringWithUTF8String:stdStr.c_str()]
+#define NSStr2CharPU(nsStr) (char*)[nsStr cStringUsingEncoding:NSUTF8StringEncoding]
+
+#define NSStr2STDStrA(nsStr) std::string([nsStr cStringUsingEncoding:NSNonLossyASCIIStringEncoding])
+
+#define NSStr2STDStrU(nsStr) std::string([nsStr cStringUsingEncoding:NSUTF8StringEncoding])
+
+#define STDStr2NSStrA(stdStr) [NSString stringWithCString:stdStr.c_str() encoding:NSASCIIStringEncoding]
+
+#define STDStr2NSStrU(stdStr) [NSString stringWithCString:stdStr.c_str() encoding:NSUTF8StringEncoding]
 
 #define STDStr2NSData(stdStr) [NSData dataWithBytes:stdStr.data() length:stdStr.size()]]
 
-#define charP2NSStr(charP) [NSString stringWithCString:charP encoding:NSUTF8StringEncoding]
+#define charP2NSStrA(charP) [NSString stringWithCString:charP encoding:NSASCIIStringEncoding]
+
+#define charP2NSStrU(charP) [NSString stringWithCString:charP encoding:NSUTF8StringEncoding]
 
 #define charP2NSData(charP) [[NSData alloc] initWithBytes:charP length:strlen(chars)]
 
 #define charV2STDStr(charV) std::string(charV.begin(), charV.end())
 
-#define NSStr2NSData(nsStr) [nsStr dataUsingEncoding:NSUTF8StringEncoding]
+#define NSStr2NSDataA(nsStr) [nsStr dataUsingEncoding:NSASCIIStringEncoding]
 
-#define NSData2NSStr(nsData) [[NSString alloc] initWithData:nsData encoding:NSASCIIStringEncoding]
+#define NSStr2NSDataU(nsStr) [nsStr dataUsingEncoding:NSUTF8StringEncoding]
 
-#define int2NSStr(i) [NSString stringWithFormat:@"%d", i]
+#define NSData2NSStrA(nsData) [[NSString alloc] initWithData:nsData encoding:NSASCIIStringEncoding]
+
+#define NSData2NSStrU(nsData) [[NSString alloc] initWithData:nsData encoding:NSUTF8StringEncoding]
