b4c7f5cb0c96c1feacef59a216cab7262c6a5cc7
Rinc Liu <i@rinc.xyz>
2017-06-19 03:49:55 -0500

    Update MemUtil.h

--- MemUtil.h
+++ MemUtil.h
@@ -1,3 +1,9 @@
 #define MALLOC(type, length) (\
 reinterpret_cast<type>(malloc(length))\
 )
+
+Byte* copyBytes(Byte* src, long length) {
+    Byte* bytes = MALLOC(Byte*, length);
+    memcpy(bytes, src, length);
+    return bytes;
+}
